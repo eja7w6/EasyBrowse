7244182899f401710fab650cfe195d31f8fdeea6
Evan Anderson <eja7w6@mail.umkc.edu>
2018-02-16 18:51:15 -0600

    Updated launch icons, first draft UI

--- app/src/main/java/evan/anderson/easybrowse/BrowseActivity.java
+++ app/src/main/java/evan/anderson/easybrowse/BrowseActivity.java
@@ -3,10 +3,12 @@ package evan.anderson.easybrowse;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class BrowseActivity extends AppCompatActivity {
+public class BrowseActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browse);
     }
